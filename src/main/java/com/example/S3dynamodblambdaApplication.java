e90db826a189fe9004c0d80e0f527e98e1d6c70f
Pavlo Balandin <pashabalandin1200@gmail.com>
2023-10-10 11:18:51 +0300

    Initialize project

---
+++ src/main/java/com/example/S3dynamodblambdaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3dynamodblambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3dynamodblambdaApplication.class, args);
+	}
+
+}
